e4c269661901fad3a2a281fe050b890b2a76c02d
Tyler Stanish <tystanish@gmail.com>
2020-05-24 01:30:18 -0500

    cleaned up parse_args and cli stuff

--- replica/src/cli.rs
+++ replica/src/cli.rs
@@ -1,24 +1,22 @@
-use std::path::Path;
-use clap::{Arg, App, SubCommand};
-use crate::index::{self, LocalIndex};
+use clap::{Arg, ArgMatches, App, SubCommand};
+use crate::index::LocalIndex;
 
 pub fn run_cli() {
-    let args = parse_args();
-}
-
-fn parse_args() {
-    let matches = App::new("Local redundancy indexer")
-        .subcommand(SubCommand::with_name("index")
-            .arg(Arg::with_name("directories")
-                 .multiple(true))
-            .help("Index the directories"))
-        .get_matches();
+    let matches = parse_args();
     if let Some(matches) = matches.subcommand_matches("index").and_then(|matches| matches.values_of("directories")) {
         let mut local_index = LocalIndex::new();
         for dir in matches {
             local_index.index(dir);
-            //index::hash_file(&dir.to_string());
         }
         println!("{}", local_index.to_json())
     }
+}
+
+fn parse_args<'a>() -> ArgMatches<'a> {
+    App::new("Local redundancy indexer")
+        .subcommand(SubCommand::with_name("index")
+            .arg(Arg::with_name("directories")
+                 .multiple(true))
+            .help("Index the directories"))
+        .get_matches()
 }
--- replica/src/main.rs
+++ replica/src/main.rs
@@ -1,12 +1,7 @@
-use std::path::Path;
-
 mod cli;
 mod index;
 
 
 fn main() {
-    let p = Path::new(".");
     cli::run_cli();
-    //hash(&p);
-    //watch(&p);
 }
